6d1d1b979d8b4aaba3fb9b06b6266803c915e4cc
SirHillsDev <Danielibetohillary@gmail.com>
2023-10-21 18:37:54 +0100

    updated cell model

--- src/main/java/com/priestdev/chumag/entity/CellLeader.java
+++ src/main/java/com/priestdev/chumag/entity/CellLeader.java
@@ -3,11 +3,9 @@ package com.priestdev.chumag.entity;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Set;
 
 @Entity
 @RequiredArgsConstructor
@@ -31,4 +29,14 @@ public class CellLeader {
     private boolean hasCompletedEncounter;
     private boolean hasCompletedDLI;
     private boolean hasADepartment;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinTable(name = "CELL_COVERAGE",
+            joinColumns = {
+                    @JoinColumn(name = "CELL_ID")
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(name = "COVERAGE_ID")
+            }
+    )
+    private Set<CoverageArea> coverageAreaSet;
 }
--- src/main/java/com/priestdev/chumag/entity/FirstTimer.java
+++ src/main/java/com/priestdev/chumag/entity/FirstTimer.java
@@ -18,6 +18,7 @@ public class FirstTimer {
     private Long id;
     private String firstName;
     private String lastName;
+    private String names;
     private String emailAddress;
     private String address;
     private String area;
